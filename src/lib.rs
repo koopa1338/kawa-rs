af6b17cde7eed29ebc3317d424fd51317aebed05
koopa1338 <sinner1991@gmail.com>
2021-01-04 05:14:17 +0100

    removed authentication and main fn
    first get downloading files without authentication with a usable ui
    working.

--- src/lib.rs
+++ src/lib.rs
@@ -1,71 +1,43 @@
-use reqwest::{Client, Result, Response};
-use std::collections::HashMap;
+use reqwest::{Response, Result};
 use std::path::Path;
 
+#[allow(dead_code)]
 pub struct Account<'a> {
     username: &'a str,
     password: &'a str,
-    pub premium: bool,
+    premium: bool,
 }
 
 impl Account<'_> {
     pub fn new<'a>(username: &'a str, password: &'a str) -> Account<'a> {
-        let mut acc = Account {
+        Account {
             username,
             password,
             premium: false,
-        };
-        acc.get_premium_status();
-        acc
+        }
     }
 
     pub fn get_premium_status(&mut self) {
-        let prem = false;
-        self.premium = prem;
+        unimplemented!();
     }
 
     pub async fn authenticate(&self) -> Result<Response> {
-        // TODO: make a post request to login and save the session cookie
-
-        /* Host: ddownload.com
-         * Params login=<username>&password=<HTMLEncodedPassword>
-         */
-        let mut params = HashMap::new();
-        params.insert("login", self.username);
-        params.insert("password", self.password);
-        let client = Client::builder()
-            .cookie_store(true)
-            .build()
-            .unwrap();
-        let resp = client
-            .post("https://ddownload.com/login.html")
-            .form(&params)
-            .send()
-            .await?;
-        println!("{:#?}", resp);
-        for cookie in resp.cookies() {
-            println!(
-                "COOKIE JAR: name = {}, value = {}",
-                cookie.name(),
-                cookie.value()
-            );
-        }
-        Ok(resp)
+        unimplemented!();
     }
 }
 
 #[allow(dead_code)]
-pub struct Package<'a> {
+pub struct Package {
     name: String,
-    parts: Vec<&'a Part<'a>>,
+    parts: Vec<Part>,
     progress: f64,
     size: f64,
 }
 
 #[allow(dead_code)]
-impl Package<'_> {
-    fn new<'a>(name: String, parts: Vec<&'a Part>, progress: f64, size: f64) -> Package<'a> {
-        Package {
+impl Package {
+    fn new(name: String, parts: Vec<Part>, progress: f64, size: f64) -> Self {
+        Self {
             name,
             parts,
             progress,
@@ -75,20 +47,18 @@ impl Package<'_> {
 }
 
 #[allow(dead_code)]
-pub struct Part<'a> {
-    hoster: String,
+pub struct Part {
     name: String,
-    path: &'a Path,
+    path: Box<Path>,
     progress: f64,
     size: f64,
     state: State,
 }
 
 #[allow(dead_code)]
-impl Part<'_> {
-    fn new<'a>(hoster: String, name: String, path: &'a Path, progress: f64, size: f64, state: State) -> Part<'a> {
-        Part {
-            hoster,
+impl Part {
+    fn new(name: String, path: Box<Path>, progress: f64, size: f64, state: State) -> Self {
+        Self {
             name,
             path,
             progress,
@@ -97,7 +67,6 @@ impl Part<'_> {
         }
     }
 }
-
 
 #[allow(dead_code)]
 enum State {
--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,6 @@
-/*
- * Prototype of a OCH downloader in rust.
- *
- */
-
-mod account;
-
-//use std::fs::File;
-//use std::io::prelude::*;
-use account::Account;
-use std::path::Path;
-
-pub struct DownloadList<'a> {
-    _entries: Vec<DownloadEntries<'a>>,
-}
-
-pub struct DownloadEntries<'a> {
-    _url: &'a str,
-    _filename: &'a str,
-}
-
-pub fn download(_url: &str, _path: &Path, account: &Account) {
-    if account.premium {
-        let _session = account.authenticate();
-    } else {
-        println!("No authentication, loading in free mode...");
-    }
-
-    // TODO: download file from url
-
-    // FIXME: download data to buffer data
-
-    /*
-     * writes data to file
-     *
-     * let display = path.display();
-     * let mut file = match File::create(&path) {
-     *     Err(why) => panic!("couldn't create {}: {}", display, why.to_string()),
-     *     Ok(file) => file,
-     * };
-     *
-     * match file.write_all(data.as_bytes()) {
-     *     Err(why) => panic!("couldn't write to {}: {}", display, why.to_string()),
-     *     Ok(_) => println!("successfully wrote to {}", display),
-     * }
-     */
-}
+use std::error::Error;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // TODO: Read username, password and download path from a config file
-    let username: &str = "username";
-    let password: &str = "password";
-
-    // TODO: use a database to store filename and url of added items
-    let path = Path::new("./file");
-    let url: &str = "url";
-
-    let acc = Account::new(username, password);
-    acc.authenticate().await?;
-    download(&url, &path, &acc);
-    //println!("DONE");
-    Ok(())
+async fn main() -> Result<(), Box<dyn Error>> {
+    todo!();
 }
