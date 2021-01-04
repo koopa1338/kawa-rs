6e71476176fb39dfba93f1b35d7d7cb093f39ac0
koopa1338 <sinner1991@gmail.com>
2021-01-04 03:50:04 +0100

    structs for packages and parts

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-extern crate reqwest;
+use reqwest::{Client, Result, Response};
+use std::collections::HashMap;
+use std::path::Path;
 
 pub struct Account<'a> {
     username: &'a str,
@@ -22,16 +24,16 @@ impl Account<'_> {
         self.premium = prem;
     }
 
-    pub async fn authenticate(&self) -> reqwest::Result<reqwest::Response> {
+    pub async fn authenticate(&self) -> Result<Response> {
         // TODO: make a post request to login and save the session cookie
 
         /* Host: ddownload.com
          * Params login=<username>&password=<HTMLEncodedPassword>
          */
-        let mut params = std::collections::HashMap::new();
+        let mut params = HashMap::new();
         params.insert("login", self.username);
         params.insert("password", self.password);
-        let client = reqwest::Client::builder()
+        let client = Client::builder()
             .cookie_store(true)
             .build()
             .unwrap();
@@ -50,4 +52,55 @@ impl Account<'_> {
         }
         Ok(resp)
     }
+}
+
+#[allow(dead_code)]
+pub struct Package<'a> {
+    name: String,
+    parts: Vec<&'a Part<'a>>,
+    progress: f64,
+    size: f64,
+}
+
+#[allow(dead_code)]
+impl Package<'_> {
+    fn new<'a>(name: String, parts: Vec<&'a Part>, progress: f64, size: f64) -> Package<'a> {
+        Package {
+            name,
+            parts,
+            progress,
+            size,
+        }
+    }
+}
+
+#[allow(dead_code)]
+pub struct Part<'a> {
+    hoster: String,
+    name: String,
+    path: &'a Path,
+    progress: f64,
+    size: f64,
+    state: State,
+}
+
+#[allow(dead_code)]
+impl Part<'_> {
+    fn new<'a>(hoster: String, name: String, path: &'a Path, progress: f64, size: f64, state: State) -> Part<'a> {
+        Part {
+            hoster,
+            name,
+            path,
+            progress,
+            size,
+            state,
+        }
+    }
+}
+
+
+#[allow(dead_code)]
+enum State {
+    Online,
+    Offline,
 }
