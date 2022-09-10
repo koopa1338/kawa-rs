1b4c047a994d36d5afaabc988f8ae193a49aff49
koopa1338 <sinner1991@gmail.com>
2022-09-10 16:14:45 +0200

    simplified project structure for authentication

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,3 +1,2 @@
-pub mod authentication;
 pub mod gui;
 pub mod models;
--- src/lib/models/account.rs
+++ src/lib/models/account.rs
@@ -4,12 +4,28 @@ use serde::{Deserialize, Serialize};
 pub struct Account {
     pub username: String,
     pub password: String,
-    pub premium: bool,
     pub provider: AccountProvider,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
 pub enum AccountProvider {
-    Ddownload,
-    UploadedTo,
+    Ddownload { premium: bool },
+    UploadedTo { premium: bool },
+}
+
+impl Account {
+    pub fn new(username: String, password: String, provider: AccountProvider) -> Self {
+        Self {
+            username,
+            password,
+            provider,
+        }
+    }
+
+    pub fn auth_with_account_data(&mut self) {
+        match self.provider {
+            AccountProvider::Ddownload { premium } => todo!(),
+            AccountProvider::UploadedTo { premium } => todo!(),
+        }
+    }
 }
